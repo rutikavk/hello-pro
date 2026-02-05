c228047dee519a6e9bfe96226255891613b6ad88
rutikavk <rutikakenchannavar@gmail.com>
2026-02-05 15:11:50 +0530

    Add dictionary add set examples (day 4)

---
+++ day4_dictionaries_sets.py
@@ -0,0 +1,7 @@
+friend_a={"Python","Cooking","Hiking","Movies"}
+friend_b={"Hiking","Gaming","Photography","Python"}
+print(friend_a & friend_b)
+
+print(friend_a | friend_b)
+
+print(friend_a - friend_b)
