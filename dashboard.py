992f746715defb68d103b42ec260b6e9147688de
rutikavk <rutikakenchannavar@gmail.com>
2026-02-13 15:43:24 +0530

    Adding the matplotlib and pyplots

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+plt.subplot(1,2,1)
+categories=['Electronics','Clothing','Home']
+values=[300,450,200]
+plt.bar(categories,values)
+plt.subplot(1,2,2)
+plt.plot(categories,values)
+plt.tight_layout()
+plt.show()
